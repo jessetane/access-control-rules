af89db1548d30b27350c11f75727b1186342344f
Jesse Tane <jesse.tane@gmail.com>
2015-06-26 13:23:08 -0400

    async

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-exports.read = function (rules, params, keys) {
-  var allow = false
+exports.read = function (rules, params, keys, cb, allow) {
+  allow = allow || false
   params = params || {}
 
-  while (keys.length && rules) {
+  if (rules && keys.length) {
     var key = keys[0]
     var rulesForKey = !key ? rules : rules[key]
 
@@ -20,28 +20,40 @@ exports.read = function (rules, params, keys) {
     if (rulesForKey) {
       var rule = rulesForKey['.read']
       if (rule === false) {
-        return false
-      } else if (typeof rule === 'function' && !rule.call(params)) {
-        return false
+        cb(null, false)
+        return
+      } else if (typeof rule === 'function') {
+        rule.call(params, function (err, _allow) {
+          if (err) return cb(err)
+          if (!_allow) return cb(null, _allow)
+
+          rules = rulesForKey
+          keys.shift()
+          exports.read(rules, params, keys, cb, _allow)
+        })
+        return
       } else if (rule) {
         allow = true
       }
     } else {
-      return allow
+      cb(null, allow)
+      return
     }
 
     rules = rulesForKey
     keys.shift()
+    exports.read(rules, params, keys, cb, allow)
+    return
   }
 
-  return allow
+  cb(null, allow)
 }
 
-exports.write = function (rules, params, keys, value, allow) {
+exports.write = function (rules, params, keys, value, cb, allow) {
   allow = allow || false
   params = params || {}
 
-  while (keys.length && rules) {
+  if (rules && keys.length) {
     var key = keys[0]
     var rulesForKey = !key ? rules : rules[key]
 
@@ -59,29 +71,51 @@ exports.write = function (rules, params, keys, value, allow) {
     if (rulesForKey) {
       var rule = rulesForKey['.write']
       if (rule === false) {
-        return false
-      } else if (typeof rule === 'function' && !rule.call(params, value)) {
-        return false
+        cb(null, false)
+        return
+      } else if (typeof rule === 'function') {
+        rule.call(params, value, function (err, _allow) {
+          if (err) return cb(err)
+          if (!_allow) return cb(null, _allow)
+
+          rules = rulesForKey
+          keys.shift()
+          exports.write(rules, params, keys, value, cb, _allow)
+        })
+        return
       } else if (rule) {
         allow = true
       }
     } else {
-      return allow
+      cb(null, allow)
+      return
     }
 
     rules = rulesForKey
     keys.shift()
+    exports.write(rules, params, keys, value, cb, allow)
+    return
   }
 
   if (allow && value && typeof value === 'object') {
+    var n = Object.keys(value).length
     for (key in value) {
+      if (!cb) return
       var subparams = {}
       for (var p in params) subparams[p] = params[p]
-      if (!exports.write(rules, subparams, [ key ], value[key], allow)) {
-        return false
-      }
+      exports.write(rules, subparams, [ key ], value[key], function (err, _allow) {
+        if (err || !_allow) {
+          cb && cb(err, _allow)
+          cb = null
+          return
+        }
+        if (--n === 0) {
+          cb && cb(null, allow)
+        }
+      }, allow)
     }
+    return
   }
 
-  return allow
+  cb(null, allow)
 }
